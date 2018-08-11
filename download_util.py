a6ba306b9c7cb478d0e08fde5f1dc727204531c3
Us <yos.virtus@gmail.com>
2018-08-11 21:32:21 +0300

    Give the hook function a better name

--- download_util.py
+++ download_util.py
@@ -1,7 +1,7 @@
 import urllib.request
 import sys
 
-def dl_progress(count, blockSize, totalSize):
+def download_progress(count, blockSize, totalSize):
     percent = int(count*blockSize*100/totalSize)
     sys.stdout.write("\r" + file_url + "...%d%%" % percent)
     sys.stdout.flush()
@@ -12,5 +12,5 @@ print("================================================================")
 
 for n in range(45, 49):
     file_url = "{0}{1}.{2}".format(files_base_url, n, ext)
-    urllib.request.urlretrieve(file_url, "{0}.mp4".format(n), reporthook=dl_progress)
+    urllib.request.urlretrieve(file_url, "{0}.mp4".format(n), reporthook=download_progress)
     print("")
